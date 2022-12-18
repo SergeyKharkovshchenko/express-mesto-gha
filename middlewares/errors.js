4d19f19749d2d42471bcf8b04e8d7546859013f2
Sergey Kharkovshchenko <kharkovchenko@gmail.com>
2022-12-18 14:56:38 +0100

    добавил в ПР 14 исправления 1

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -1,15 +1,15 @@
 const BadRequestError = require('./BadRequestError');
-const ItemNotFoundError = require('./ItemNotFoundError');
+// const ItemNotFoundError = require('./ItemNotFoundError');
 const ServerError = require('./ServerError');
 
 // // eslint-disable-next-line max-classes-per-file
-// class ItemNotFoundError extends Error {
-//   constructor(message) {
-//     super(message);
-//     this.name = 'ItemNotFoundError';
-//     this.statusCode = 404;
-//   }
-// }
+class ItemNotFoundError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'ItemNotFoundError';
+    this.statusCode = 404;
+  }
+}
 
 // class BadRequestError extends Error {
 //   constructor(message) {
