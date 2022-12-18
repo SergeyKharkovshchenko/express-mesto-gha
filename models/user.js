0eef6d2bebe03a3fe68cde119f3214e2304892b0
Sergey Kharkovshchenko <kharkovchenko@gmail.com>
2022-12-18 14:42:12 +0100

    добавил в ПР 14 исправления 1

--- models/user.js
+++ models/user.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 // const isURL = require('validator');
-// const { default: isEmail } = require('validator/lib/isEmail');
+const { default: isEmail } = require('validator/lib/isEmail');
 
 const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
     unique: true,
-    // validate: {
-    //   validator: (email) => isEmail(email),
-    //   message: (props) => `Неверный емейл: ${props.value}`,
-    // },
+    validate: {
+      validator: (email) => isEmail(email),
+      message: (props) => `Неверный емейл: ${props.value}`,
+    },
   },
   password: {
     type: String,
--- routes/cards.js
+++ routes/cards.js
@@ -9,14 +9,11 @@ router.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     link: Joi.string().required().min(2).max(100)
-      .regex(/https:\/\/.*/),
+      .regex(/https{0,1}:\/\/.*/),
   }),
 }), createCard);
-
-router.delete('/:cardId', celebrate({ params: Joi.object().keys({ cardId: Joi.string().length(24).required().regex(/[A-Fa-f0-9]*/) }) }), deletCardById);
-// .regex(/[A-Fa-f0-9]/)
-
-router.put('/:cardId/likes', celebrate({ params: Joi.object().keys({ cardId: Joi.string().length(24).required().regex(/[A-Fa-f0-9]*/) }) }), likeCard);
-router.delete('/:cardId/likes', celebrate({ params: Joi.object().keys({ cardId: Joi.string().length(24).required().regex(/[A-Fa-f0-9]*/) }) }), dislikeCard);
+router.delete('/:cardId', celebrate({ params: Joi.object().keys({ cardId: Joi.string().length(24).required().hex() }) }), deletCardById);
+router.put('/:cardId/likes', celebrate({ params: Joi.object().keys({ cardId: Joi.string().length(24).required().hex() }) }), likeCard);
+router.delete('/:cardId/likes', celebrate({ params: Joi.object().keys({ cardId: Joi.string().length(24).required().hex() }) }), dislikeCard);
 
 module.exports = router;
