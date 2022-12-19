c6f43e9271a7e271878c586c03b10568e0effa9e
Sergey Kharkovshchenko <kharkovchenko@gmail.com>
2022-12-19 16:20:15 +0100

    загрузил пр 14 после проверки 1

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -1,43 +1,8 @@
-// eslint-disable-next-line max-classes-per-file, import/no-unresolved, import/extensions
 const { BadRequestError } = require('./BadRequestError');
-
-// class BadRequestError extends Error {
-//   constructor(message) {
-//     super(message);
-//     this.statusCode = 400;
-//   }
-// }
-
-class ServerError extends Error {
-  constructor(message) {
-    super(message);
-    this.statusCode = 500;
-  }
-}
-
-class UnauthorizedError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = 'UnauthorizedError';
-    this.statusCode = 401;
-  }
-}
-
-class ItemNotFoundError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = 'ItemNotFoundError';
-    this.statusCode = 404;
-  }
-}
-
-class AccessDeniedError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = 'AccessDeniedError';
-    this.statusCode = 403;
-  }
-}
+const { ServerError } = require('./ServerError');
+const { UnauthorizedError } = require('./UnauthorizedError');
+const { ItemNotFoundError } = require('./ItemNotFoundError');
+const { AccessDeniedError } = require('./AccessDeniedError');
 
 module.exports = {
   BadRequestError,
