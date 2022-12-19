6fe820137e0d486126924678184616182df94b24
Sergey Kharkovshchenko <kharkovchenko@gmail.com>
2022-12-19 14:17:22 +0100

    загрузил пр 14 после проверки 1

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -13,13 +13,13 @@ const BadRequestError = require('./BadRequestError');
 //   }
 // }
 
-class ItemNotFoundError extends Error {
+const ItemNotFoundError = class ItemNotFoundError extends Error {
   constructor(message) {
     super(message);
     this.name = 'ItemNotFoundError';
     this.statusCode = 404;
   }
-}
+};
 
 module.exports = {
   BadRequestError,
