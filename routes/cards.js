728f71eeaa69026c53e036f6e1339b7a97059f16
Sergey Kharkovshchenko <kharkovchenko@gmail.com>
2022-12-20 17:10:13 +0100

    загрузил пр 14 после проверки 2

--- routes/cards.js
+++ routes/cards.js
@@ -8,7 +8,7 @@ router.get('/', getAllCards);
 router.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().min(2).max(100)
+    link: Joi.string().required()
       .regex(/https{0,1}:\/\/.*/),
   }),
 }), createCard);
